bd8df141366f67f1e0cc1971cb187133cf6594b7
santii810 <santii810@hotmail.com>
2016-02-18 13:32:07 +0100

    space invaders

--- VisualStudio/ADAT_UD4_EJ1/ADAT_UD4_EJ1/Migrations/Configuration.cs
+++ VisualStudio/ADAT_UD4_EJ1/ADAT_UD4_EJ1/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ namespace ADAT_UD4_EJ1.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ADAT_UD4_EJ1.Model.Context>
+    internal sealed class Configuration : <ADAT_UD4_EJ1.Model.Context>
     {
         public Configuration()
         {
